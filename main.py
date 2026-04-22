95b749a4d19519f839e6853f679da3b84f6a2ddc
Eng-Ahmed-Farouk-backup <ahmed.farouk@adapt-eg.org>
2026-04-22 02:46:50 +0200

    Added Signup
    I added the Sign up Option and Finished the accounts Function.

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
+import sys
 print("Welcome To ATM System")
 usernames = ["ahmed"]
 passwords = {"ahmed" : "test"}
-auth = str
-username1 = str
+
+    
 def accounts() :
-    ac = input("Enter the number of your choice : 1.Login 2.Signup 2.Exit ::: ")
+    ac = input("Enter the number of your choice : 1.Login 2.Signup 3.Exit ::: ")
     if ac == "3" :
-        while True :
-            break
+        sys.exit()
     if ac == "1" :
         username1 = input("Enter your Username : ")
         if username1 in usernames :
             passw = input("Enter your Password : ")
             if passw == passwords[username1] :
-                auth = "true"
+                print("you Logged in !")
+                main()
             else :
                 print("Wrong Password Please Try again")
         else :
             print("Wrong Username please try again")
+    if ac == "2" :
+        username1 = "null" # store the intger with null cuz I can't return an empty Variable.
+        auth = "null" #same as username1
+        username2 = input("Please Enter a Username : ")
+        usernames.append(username2)
+        passwo = input("Please Enter a Password :::::  ")
+        passwords[username2] = passwo
+        print("account Created ! please Login.")
     return username1 , auth
