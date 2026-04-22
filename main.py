f15792b5b6949d43af43bd074babf1678ab612fc
Eng-Ahmed-Farouk-backup <ahmed.farouk@adapt-eg.org>
2026-04-22 17:00:48 +0200

    Added Main Function
    Added Main function and deleted auth var. finished the first part of the code

--- main.py
+++ main.py
@@ -2,29 +2,55 @@ import sys
 print("Welcome To ATM System")
 usernames = ["ahmed"]
 passwords = {"ahmed" : "test"}
-
+balances = {"ahmed" : 500}
     
 def accounts() :
     ac = input("Enter the number of your choice : 1.Login 2.Signup 3.Exit ::: ")
     if ac == "3" :
         sys.exit()
-    if ac == "1" :
+    elif ac == "1" :
         username1 = input("Enter your Username : ")
         if username1 in usernames :
             passw = input("Enter your Password : ")
             if passw == passwords[username1] :
                 print("you Logged in !")
-                main()
+                main(username1)
             else :
                 print("Wrong Password Please Try again")
         else :
             print("Wrong Username please try again")
-    if ac == "2" :
-        username1 = "null" # store the intger with null cuz I can't return an empty Variable.
-        auth = "null" #same as username1
-        username2 = input("Please Enter a Username : ")
-        usernames.append(username2)
+    elif ac == "2" :
+        username1 = input("Please Enter a Username : ")
+        usernames.append(username1)
         passwo = input("Please Enter a Password :::::  ")
-        passwords[username2] = passwo
-        print("account Created ! please Login.")
-    return username1 , auth
+        passwords[username1] = passwo
+        balances[username1] = 0
+        print("account Created!")
+    return username1
+
+def main(username11) :
+    choice = input("Please enter the Number of your Choice :\n 1.Withdraw \n 2.Deposit \n 3.Check balance \n ::::  ")
+    if choice == "3" : 
+        print("Hey",username11,"your balance is :::: ",balances[username11],"USD")
+    elif choice == "2" :
+        deposit = input("Please Enter the amount of money You want to Deposit (I wish the ATM was that Easy) : ")
+        if deposit == int :
+            balances[username11] = deposit + balances[username11]
+            print("You Deposited",deposit,"USD into your balance.")
+        else :
+            print("please write a valid Number to deposit")
+    elif choice == "1" :
+        withdraw = input("Please Enter the amount of money You want to Withdraw : ")
+        if withdraw != int :
+            print("please Enter a Valid Value")
+        else :
+            if withdraw <= balances[username11] :
+                balances[username11] = balances[username11] - withdraw
+                print("You Withdrew",withdraw,"USD from your balance.")
+            else :
+                print("please Enter a Valid amount")
+
+
+while True :
+    accc = accounts()
+    main(accc)
